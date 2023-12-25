e0863d53fa148ea90bc4919fa1d1a9655465615d
Inderby <youk8253@gmail.com>
2023-12-25 18:10:43 +0900

    add `@After`, `@AfterThrowing`, `@AfterReturning`

--- src/main/java/com/excelsiorkim/aop/LoggingAspect.java
+++ src/main/java/com/excelsiorkim/aop/LoggingAspect.java
@@ -2,6 +2,10 @@ package com.excelsiorkim.aop;
 
 import ch.qos.logback.classic.Logger;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.LoggerFactory;
@@ -18,6 +22,38 @@ public class LoggingAspect {
     logger.info(
       "Before Aspect - Method called - {}",
       joinPoint.getSignature().getName()
+    );
+  }
+
+  @After("execution(* com.excelsiorkim.aop.*.*(..))")
+  public void logMethodAfter(JoinPoint joinPoint) {
+    logger.info(
+      "After Aspect - Method called - {}",
+      joinPoint.getSignature().getName()
+    );
+  }
+
+  @AfterThrowing(
+    value = "execution(* com.excelsiorkim.aop.*.*(..))",
+    throwing = "exception"
+  )
+  public void logMethodAfterThrowing(JoinPoint joinPoint, Exception exception) {
+    logger.info(
+      "After Throwing Aspect - Method called - {} - Exception - {}",
+      joinPoint.getSignature().getName(),
+      exception
+    );
+  }
+
+  @AfterReturning(
+    value = "execution(* com.excelsiorkim.aop.*.*(..))",
+    returning = "result"
+  )
+  public void logMethodAfterReturning(JoinPoint joinPoint, Object result) {
+    logger.info(
+      "After Returning Aspect - Method called - {} - Result - {}",
+      joinPoint.getSignature().getName(),
+      result
     );
   }
 }
